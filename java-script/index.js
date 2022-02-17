f1ce0145d7a086c327ce1bc013ebeadf803cb153
Ahsan Habib <ahsanpub16b@gmail.com>
2022-02-17 14:07:27 +0600

    input all id calling here

---
+++ java-script/index.js
@@ -0,0 +1,11 @@
+//calling Id here: 
+
+const inputIncome = document.getElementById('income');
+const inputFood = document.getElementById('food');
+const inputRent = document.getElementById('rent');
+const inputClothes = document.getElementById('clothes');
+const inputSaving = document.getElementById('save');
+const balance = document.getElementById('balance');
+
+
+
