44577cafba59706dafd7cdff37ff91276dc28357
Ahsan Habib <ahsanpub16b@gmail.com>
2022-02-17 14:38:29 +0600

    Calculate function with error massage working done here!

--- java-script/index.js
+++ java-script/index.js
@@ -1,3 +1,4 @@
+
 //calling Id here: 
 
 const inputIncome = document.getElementById('income');
@@ -6,6 +7,59 @@ const inputRent = document.getElementById('rent');
 const inputClothes = document.getElementById('clothes');
 const inputSaving = document.getElementById('save');
 const balance = document.getElementById('balance');
+
+
+// calculate all input value:-
+
+function calculateInput() {
+
+    if (isNaN(parseInt(inputIncome.value))) {
+        inputIncome.classList.toggle('border-red-600');
+        alert('Income input must be number...!');
+        return;
+    }
+    else if (isNaN(parseInt(inputFood.value))) {
+       
+        alert('Food input must be number...!');
+        return;
+    }
+    else if (isNaN(parseInt(inputRent.value))) {
+        
+        alert('Rent input must be number.!');
+        return;
+    }
+    else if (isNaN(parseInt(inputClothes.value))) {
+       
+        alert('Clothes input must be number.!');
+        return;
+    }
+    else if (parseInt(inputIncome.value) < 0) {
+       
+        alert('Income input must be a positive number...!')
+    }
+    else if (parseInt(inputFood.value) < 0) {
+        
+        alert('Food input must be a positive number...!');
+    }
+
+    else if (parseInt(inputRent.value) < 0) {
+        inputRent.classList.toggle('border-red-600');
+        alert('Rent input must be a positive number...!');
+    }
+    else if (parseInt(inputClothes.value) < 0) {
+        
+        alert('Clothes input must be a positive number...!');
+    }
+    else {
+        const totalExpenses = parseInt(inputFood.value) + parseInt(inputRent.value) + parseInt(inputClothes.value);
+    
+        const balance = parseInt(inputIncome.value) - totalExpenses;
+
+        
+        document.getElementById('totalExpenses').innerText = totalExpenses;
+        document.getElementById('balance').innerText = balance;
+    }
+}
 
 
 
